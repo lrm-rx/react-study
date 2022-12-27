1383447336df608716734c06b158f0b6f7a0d40b
lrm <756131502@qq.com>
2022-12-27 09:40:22 +0800

    react使用antd

--- learn-antdesign/src/App.js
+++ learn-antdesign/src/App.js
@@ -1,47 +1,44 @@
 import React, { PureComponent } from 'react'
+import moment from 'moment';
+
+import { Button, DatePicker } from 'antd';
 import { PoweroffOutlined } from '@ant-design/icons';
-import { Space, Button } from 'antd'
+
+import HYHomeRecommend from './components/home/childCpns/home-recommend';
+
 
 export default class App extends PureComponent {
   render() {
     const loadings = true;
+
     return (
       <>
-        <Space
-          style={{
-            width: '100%',
-          }}
+        <HYHomeRecommend/>
+        <Button type="primary" loading>
+          Loading
+        </Button>
+        <Button type="primary" size="small" loading>
+          Loading
+        </Button>
+        <Button type="primary" icon={<PoweroffOutlined />} loading />
+        <br />
+        <Button type="primary" loading={loadings[0]}>
+          Click me!
+        </Button>
+        <Button
+          type="primary"
+          icon={<PoweroffOutlined />}
+          loading={loadings[1]}
         >
-          <Button type="primary" loading>
-            Loading
-          </Button>
-          <Button type="primary" size="small" loading>
-            Loading
-          </Button>
-          <Button type="primary" icon={<PoweroffOutlined />} loading />
-        </Space>
-
-        <Space
-          style={{
-            width: '100%',
-          }}
-        >
-          <Button type="primary" loading={loadings[0]}>
-            Click me!
-          </Button>
-          <Button
-            type="primary"
-            icon={<PoweroffOutlined />}
-            loading={loadings[1]}
-          >
-            Click me!
-          </Button>
-          <Button
-            type="primary"
-            icon={<PoweroffOutlined />}
-            loading={loadings[2]}
-          />
-        </Space>
+          Click me!
+        </Button>
+        <Button
+          type="primary"
+          icon={<PoweroffOutlined />}
+          loading={loadings[2]}
+        />
+        <DatePicker defaultValue={moment('2015-06-06', "YYYY-MM-DD")}
+                    allowClear={false}/>
       </>
     )
   }
--- learn-antdesign/src/index.js
+++ learn-antdesign/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
+import 'moment/locale/zh-hk';
+
+// import 'antd/dist/antd.css';
+import 'antd/dist/antd.less';
+
+// import App from './App';
+import App from './comment/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
