f590133502f6c3bbf99e631c604d1172f38239ec
lrm <756131502@qq.com>
2022-12-28 15:01:39 +0800

    react-redux

---
+++ react_redux/src/pages/Home.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import store from '../store'
+import { addAction } from '../store/actionCreators';
+
+export default class Home extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: store.getState().count
+    }
+  }
+  componentDidMount() {
+    this.unsubscribue = store.subscribe(() => {
+      this.setState({
+        count: store.getState().count
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribue();
+  }
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h2>Home</h2>
+        <h2>当前计数: {count}</h2>
+        <button onClick={e => this.increment()}>+1</button>
+        <button onClick={e => this.addNumber(5)}>+5</button>
+      </div>
+    )
+  }
+  increment() {
+    store.dispatch(addAction(1))
+  }
+
+  addNumber(num) {
+    store.dispatch(addAction(num))
+  }
+}
