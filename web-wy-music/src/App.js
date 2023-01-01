d369b17a23f70d19a1ce77e6a7ba6198abe3a4df
lrm <756131502@qq.com>
2023-01-01 10:20:47 +0800

    首页header和footer

--- web-wy-music/src/App.js
+++ web-wy-music/src/App.js
@@ -1,10 +1,19 @@
 import React, { memo } from 'react'
+import { renderRoutes } from 'react-router-config'
+import { HashRouter } from 'react-router-dom'
+
+import routes from './router'
+
+import AppHeader from '@/components/app-header'
+import AppFooter from '@/components/app-footer'
 
 const App = memo(() => {
   return (
-    <div>
-      <h2>App</h2>
-    </div>
+    <HashRouter>
+      <AppHeader />
+      {renderRoutes(routes)}
+      <AppFooter />
+    </HashRouter>
   )
 })
 
