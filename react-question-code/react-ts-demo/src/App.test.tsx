71db38c4b6cfe15d70c1e6a830bc49a19cf658f4
lrm <756131502@qq.com>
2023-05-28 16:58:15 +0800

    react18项目案例初始化

--- react-question-code/react-ts-demo/src/App.test.tsx
+++ react-question-code/react-ts-demo/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Button from './index';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<Button>click me</Button>);
-  const linkElement = screen.getByText(/click me/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
