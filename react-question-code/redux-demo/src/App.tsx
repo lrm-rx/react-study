9acc140c70647032be6ae4d41aba9c62bdcb1aa7
lrm <756131502@qq.com>
2023-07-01 19:19:20 +0800

    init & redux

---
+++ react-question-code/redux-demo/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Count from './pages/Count';
+
+function App() {
+  return (
+    <>
+      <h1>
+        Redux demo
+      </h1>
+      <Count />
+    </>
+  );
+}
+
+export default App;
---
+++ react-question-code/redux-demo/src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countReducer from "./count";
+
+export type StateType = {
+  count: number;
+};
+
+export default configureStore({
+  reducer: {
+    count: countReducer,
+
+    // 扩展其他模块
+  },
+});
