71f6d3f39ad9385345215ae60c19d67b02cef561
lrm <756131502@qq.com>
2023-07-03 23:17:57 +0800

    编辑

---
+++ react-question-code/wenjuan-fe/src/pages/question/Edit/EditCanvas.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import styles from './EditCanvas.module.scss'
+
+const EditCanvas: FC = () => {
+  return <div className={styles.canvas}></div>
+}
+
+export default EditCanvas
