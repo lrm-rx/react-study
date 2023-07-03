f7be9d4db1e7673d62445d1bef371e94e1fe712f
lrm <756131502@qq.com>
2023-07-03 23:06:18 +0800

    编辑问卷 - 画布

--- react-question-code/wenjuan-fe/src/pages/question/Edit/index.tsx
+++ react-question-code/wenjuan-fe/src/pages/question/Edit/index.tsx
@@ -1,13 +1,22 @@
 import React, { FC, useEffect, useState } from 'react'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
+import styles from './index.module.scss'
 
 const Edit: FC = () => {
   const { loading, data } = useLoadQuestionData()
 
   return (
-    <div>
-      <p>Edit page</p>
-      {loading ? <p>loading</p> : <p>{JSON.stringify(data)}</p>}
+    <div className={styles.container}>
+      <div style={{ backgroundColor: '#fff' }}>Header</div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}></div>
+          </div>
+          <div className={styles.right}>right</div>
+        </div>
+      </div>
     </div>
   )
 }
