b640c1284736d10ba65ba2abbc3e3180970c75e8
lrm <756131502@qq.com>
2023-08-26 20:40:47 +0800

    删除组件

--- react-question-code/mock-server/mock/data/getComponentList.js
+++ react-question-code/mock-server/mock/data/getComponentList.js
@@ -38,58 +38,58 @@ function getComponentList() {
       props: { title: "你的电话", placeholder: "请输入电话..." },
     },
     // Textarea
-    {
-      fe_id: "c5",
-      type: "questionTextarea",
-      title: "多行输入",
-      isHidden: false,
-      isLocked: false,
-      props: { title: "你的爱好", placeholder: "请输入..." },
-    },
-    // Paragraph
-    {
-      fe_id: "c6",
-      type: "questionParagraph",
-      title: "段落",
-      isHidden: false,
-      isLocked: false,
-      props: { text: "一行段落1\n一行段落2", isCenter: false },
-    },
-    // Radio
-    {
-      fe_id: "c7",
-      type: "questionRadio",
-      title: "单选",
-      isHidden: false,
-      isLocked: false,
-      props: {
-        title: "单选标题",
-        isVertical: false,
-        options: [
-          { value: "item1", text: "选项1" },
-          { value: "item2", text: "选项2" },
-          { value: "item3", text: "选项3" },
-        ],
-        value: "",
-      },
-    },
-    // Checkbox
-    {
-      fe_id: "c8",
-      type: "questionCheckbox",
-      title: "多选",
-      isHidden: false,
-      isLocked: false,
-      props: {
-        title: "多选标题",
-        isVertical: false,
-        list: [
-          { value: "item1", text: "选项1", checked: true },
-          { value: "item2", text: "选项2", checked: false },
-          { value: "item3", text: "选项3", checked: false },
-        ],
-      },
-    },
+    // {
+    //   fe_id: "c5",
+    //   type: "questionTextarea",
+    //   title: "多行输入",
+    //   isHidden: false,
+    //   isLocked: false,
+    //   props: { title: "你的爱好", placeholder: "请输入..." },
+    // },
+    // // Paragraph
+    // {
+    //   fe_id: "c6",
+    //   type: "questionParagraph",
+    //   title: "段落",
+    //   isHidden: false,
+    //   isLocked: false,
+    //   props: { text: "一行段落1\n一行段落2", isCenter: false },
+    // },
+    // // Radio
+    // {
+    //   fe_id: "c7",
+    //   type: "questionRadio",
+    //   title: "单选",
+    //   isHidden: false,
+    //   isLocked: false,
+    //   props: {
+    //     title: "单选标题",
+    //     isVertical: false,
+    //     options: [
+    //       { value: "item1", text: "选项1" },
+    //       { value: "item2", text: "选项2" },
+    //       { value: "item3", text: "选项3" },
+    //     ],
+    //     value: "",
+    //   },
+    // },
+    // // Checkbox
+    // {
+    //   fe_id: "c8",
+    //   type: "questionCheckbox",
+    //   title: "多选",
+    //   isHidden: false,
+    //   isLocked: false,
+    //   props: {
+    //     title: "多选标题",
+    //     isVertical: false,
+    //     list: [
+    //       { value: "item1", text: "选项1", checked: true },
+    //       { value: "item2", text: "选项2", checked: false },
+    //       { value: "item3", text: "选项3", checked: false },
+    //     ],
+    //   },
+    // },
   ];
 }
 
--- react-question-code/wenjuan-fe/src/store/componentsReducer/index.ts
+++ react-question-code/wenjuan-fe/src/store/componentsReducer/index.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import produce from 'immer'
 import { ComponentPropsType } from '../../components/QuestionComponents'
+import { getNextSelectedId } from './utils'
 
 export type ComponentInfoType = {
   fe_id: string // 前端生成的 id, 服务端 Mongodb 不认这种格式, 所以自定义一个 fe_id
@@ -70,10 +71,25 @@ export const componentsSlice = createSlice({
         }
       }
     ),
+
+    // 删除选中的组件
+    removeSelectedComponent: produce((draft: ComponentsStateType) => {
+      const { componentList = [], selectedId: removedId } = draft
+      // 重新计算 selectedId
+      const newSelectedId = getNextSelectedId(removedId, componentList)
+      draft.selectedId = newSelectedId
+      const index = componentList.findIndex(c => c.fe_id === removedId)
+      componentList.splice(index, 1)
+    }),
   },
 })
 
-export const { resetComponents, changeSelectedId, addComponent, changeComponentProps } =
-  componentsSlice.actions
+export const {
+  resetComponents,
+  changeSelectedId,
+  addComponent,
+  changeComponentProps,
+  removeSelectedComponent,
+} = componentsSlice.actions
 
 export default componentsSlice.reducer
